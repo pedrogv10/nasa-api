fc2c6c956e99847ab71bd722fcdf783b471a6676
Pedro Henrique <pedrogv133@gmail.com>
2022-12-04 11:48:49 -0300

    initial conditions of media type and displaying then

---
+++ script.js
@@ -0,0 +1,39 @@
+let searchButton = document.querySelector("#search");
+
+//Add an event listener to the button that runs the function sendApiRequest when it is clicked
+searchButton.addEventListener("click", () => {
+    console.log("button pressed")
+    sendApiRequest()
+})
+
+
+//An asynchronous function to fetch data from the API.
+async function sendApiRequest() {
+    let API_KEY = "NbrFmVqi4X50ahB3EcoXqni5L3t1kUyauuwsfVOK";
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
+    console.log(response);
+    let data = await response.json();
+    console.log(data);
+
+    useApiData(data)
+}
+
+
+//function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
+function useApiData(data) {
+
+    if (data.media_type == "video") {
+        console.log('é video')
+
+        document.querySelector('#content').innerHTML = 
+        `<iframe width="420" height="315"
+        src="${data.url}">
+        </iframe>`;        
+    }
+
+    if (data.media_type == "image") {
+        console.log('é imagem');
+        document.querySelector('#content').innerHTML += `<img src="${data.url}" />`;
+    }
+}
+
